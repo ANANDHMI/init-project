104cc2a05a996a68a577e5d2b1bac435ece8021c
anandh2001 <aanandh1192@gmail.com>
2023-07-09 02:26:11 +0530

    3. spread operation , template string

---
+++ src/3-spreadOperator/init2.js
@@ -0,0 +1,37 @@
+// let numbers=[1,2,3,4]
+
+// const numbers2=[5,6,7]
+
+// numbers=[...numbers,...numbers2]
+
+// console.log(numbers2)
+
+// //spread operation
+// console.log(...numbers2)
+
+
+//object creation
+
+// let obj={
+
+//     name:'user',
+//     age:'10'
+// }
+
+// // //add
+// // obj={...obj,id:'1'}
+
+// //update
+// obj={...obj,name:'test'}
+
+// console.log(obj)
+
+
+let name='A'
+let tech='React'
+
+//normal string concate
+console.log('Hello '+ name+ 'welcome to ' +tech)
+
+//template string
+console.log(`Hello ${name} welcome to ${tech}`)
